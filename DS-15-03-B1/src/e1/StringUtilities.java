676ca70279d02e617087c7a5e3c98db0aae872e2
miguel <miguel.rodriguezp@udc.es>
2020-10-22 16:50:14 +0200

    prueba

--- DS-15-03-B1/src/e1/StringUtilities.java
+++ DS-15-03-B1/src/e1/StringUtilities.java
@@ -5,68 +5,57 @@ import java.util.Random;
 
 public class StringUtilities {
 
-    String a = "Bye";
-    String b = "World";
-    public static boolean isValidMix ( String a , String b , String c ) {
+    public static boolean isValidMix(String a, String b, String c) {
 
-    char [] a1 = a.toCharArray();
-    char [] b1 = b.toCharArray();
-    char [] c1 = c.toCharArray();
-    
-    boolean result = true ;
+        char[] a1 = a.toCharArray();
+        char[] b1 = b.toCharArray();
+        char[] c1 = c.toCharArray();
 
-    int x=0; //tocho
-    int y=0; //bye
-    int z=0; //world
-        if(c.length() > a.length() + b.length())
-        {
+        boolean result = true;
+
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        if (c.length() > a.length() + b.length()) {
             return false;
         }
-        if(c.length() < a.length() + b.length())
-        {
+        if (c.length() < a.length() + b.length()) {
             return false;
         }
 
-    while(x<a.length() + b.length() && result){
-        if (y<a.length() && c1[x] == a1[y] ){
-            x++;
-            y++;
+        while (x < a.length() + b.length() && result) {
+            if (y < a.length() && c1[x] == a1[y]) {
+                x++;
+                y++;
+            } else if (z < b.length() && c1[x] == b1[z]) {
+                x++;
+                z++;
+            } else {
+                result = false;
+            }
         }
-        else if(z<b.length()&& c1[x]==b1[z]){
-            x++;
-            z++;
-        }else {
-            result = false;
-        }
+        return result;
     }
 
-    return result;
-    /**
-     * Generates a random valid mix for two Strings
-     * @param a First String to be mixed
-     * @param b Second String to be mixed
-     * @return A String that is a random valid mix of the other two .
-     */
-    }
-    public static String generateRandomValidMix (String a , String b) {
-
+    public static String generateRandomValidMix(String a, String b) {
 
         Random random = new Random();
-        int ap = a.length(),bp = b.length();
-        StringBuilder sb = new StringBuilder(ap+bp);
-        int i=0;
-        int c1 = 0, c2=0;
+        int ap = a.length(), bp = b.length();
+        StringBuilder sb = new StringBuilder(ap + bp);
+        int i = 0;
+        int c1 = 0, c2 = 0;
 
 
-        while(i<ap+bp)
-        {
-            if(random.nextInt()%2==0&& c1<ap){
+        while (i < ap + bp) {
+            if (random.nextInt() % 2 == 0 && c1 < ap) {
                 sb.append(a.charAt(c1));
-                c1++; i++;
+                c1++;
+                i++;
             }
-            if (c2<bp){
+            if (c2 < bp) {
                 sb.append(b.charAt(c2));
-                c2++; i++;
+                c2++;
+                i++;
             }
         }
 
--- DS-15-03-B1/src/e2/Code.java
+++ DS-15-03-B1/src/e2/Code.java
@@ -2,22 +2,14 @@ package e2;
 
 
 public class Code {
-    /**
-     * Determines whether a keypad is valid . To do this , it must be a rectangle and
-     * the numbers must follow the alphanumeric sequence indicated . If the array
-     * (or any of its subarrays ) is null it will be returned false .
-     *
-     * @param keypad The keypad to be analyzed .
-     * @return true if it is valid , false otherwise .
-     */
+
     public static boolean isKeypadValid(char[][] keypad) {
 
-
+        //Comprobación de nulos
         if(keypad == null)
         {
             return false;
         }
-        //Show del pad
         //(int x = 0; x < keypad.length; x++)
         for (char[] chars : keypad) {
             if (chars == null) {
@@ -30,7 +22,6 @@ public class Code {
             }
         }
 
-
         int i = 1, j = 1;
         int il = keypad.length;
         int jl = keypad[0].length;
@@ -41,13 +32,11 @@ public class Code {
             return true;
         }
 
-
         for (i = 0; i < keypad.length; i++) {
             for (j = 0; j < keypad[i].length; j++) {
-                System.out.println(""); //creo el array
+                System.out.println("");
             }
         }
-
             //numero esquina super izquierda
             if (keypad[0][0] != '1') {
                 return false;
@@ -68,37 +57,24 @@ public class Code {
                             j++;
                     }
                     i++;
-                    //System.out.println("V. Finish(numbers): ("+i+")"+"("+j+")"+"keypad:" + keypad[i][j]);
+
 
                 }
+                i--;
 
-                if (i < il) {
-                    i--;
-                }
-                //System.out.println("V. Finish(numbers): ("+i+")"+"("+j+")"+"keypad:" + keypad[i][j]);
-
-                //System.out.println("V. FFFFFFF(numbers): ("+i+")"+"("+j+")"+" keypad:" + keypad[i][j]);
-
-                if (i == il) {
-                    i--;
-                }
                 if (j == jl) {
                     j--;
                 }
-
                 if(keypad[il-1][jl-1] == '9')
                 {
                     return true;
                 }
-
                 if (j == jl-1) {
 
                     i++;
                     j = 0;
                 }
-
                 if (contador == 10 && keypad[i][j] != '0') {
-
                     return false;
                 } else {
                     if (j == jl && i<il-1) {
@@ -119,15 +95,8 @@ public class Code {
                 }
 
                 return true;
-
-
-
             } else if (keypad[1][0] == '2') {
 
-                // 1 3 5 7
-                // 2 4 6 8
-                //i = filas
-                //j = columnas
                 j = 0;
                 while (j < keypad[0].length && contador < 10) {
                     i = 0;
@@ -140,16 +109,13 @@ public class Code {
                     }
                     j++;
                 }
-                // System.out.println("V. Finish(numbers): ("+i+")"+"("+j+")"+"keypad:" + keypad[i][j]);
                 j--;
-                //System.out.println("V. FFFFFFF(numbers): ("+i+")"+"("+j+")"+" keypad:" + keypad[i][j]);
+
 
                 if (i == il) {
                     i = 0;
                     j++;
                 }
-                //System.out.println("V. AAAAAA(numbers): (" + i + ")" + "(" + j + ") " + keypad[i][j] + " contador: " + contador);
-
 
                 if (contador == 10 && keypad[i][j] != '0') {
                     return false;
@@ -166,42 +132,20 @@ public class Code {
                     }
 
                 }
-
                 return true;
-                // }
-
-                // 1 2 3 4
-                // 5 6 7 8
-                //Comprobacion rectangulo
-
             }
-
-
-            /**
-             * Given a keypad and an array of movements , it returns a String with the code
-             * resulting from applying the movements on the keypad .
-             * @param keypad alphanumeric keypad .
-             * @param movements Array with the different movements to be made for each
-            number of the key .
-             * @return Resulting code .
-             * @throws IllegalArgumentException if the keypad of the movements are invalid .
-             */
             return true;
         }
 
          public static String obtainCode ( char [][] keypad , String [] movements ) {
 
             StringBuilder resultado = new StringBuilder();
-            int lm = movements.length;
             int j = 0;
             int i = 0;
-             int il = keypad.length;
-             int jl = keypad[0].length;
             if(!isKeypadValid(keypad))
             {
                 throw new IllegalArgumentException("El keypad no es válido");
             }
-
 
             for(String x : movements)
             {
@@ -251,15 +195,6 @@ public class Code {
             return resultado.toString();
          }
 
-
-    /**
-     * Checks if an array of movements is valid when obtaining a key on a keypad .
-     * An array of movements is valid if it is formed by Strings that only have the
-     * four capital letters U, D, L and R. If the array of Strings or any of the
-     * Strings is null it will be returned false .
-     * @param movements Array of Strings representing movements .
-     * @return true if it is valid , false otherwise .
-     */
     public static boolean areMovementsValid(String[] movements) {
 
 
@@ -285,71 +220,6 @@ public class Code {
         }
        return true;
     }
-
-    public static void main(String[] args) {
-        //char[][] matriz = {{'1'}, {'2'}, {'3'}, {'4'}, {'5'}, {'6'}, {'7'}, {'8'}, {'9'}, {'0'}, {'A'}, {'B'}};
-       /* char[][] matriz
-                = {{'1', '5', '9', 'C'},
-                   {'2', '6', '0', 'D'},
-                   {'3', '7', 'A', 'E'},
-                   {'4', '8', 'B', 'F'}};*/
-               /* char[][] matriz
-                = {{'1', '4', '7', '0'},
-                   {'2', '5', '8', 'A'},
-                   {'3', '6', '9', 'B'}};*/
-              /* char[][] matriz
-                = {{'1', '3', '5', '7','9','A'},
-                   {'2', '4', '6', '8','0','B'}};*/
-           /*     char[][] matriz
-                = {{'1', '2', '3', '4'},
-                   {'5', '6', '7', '8'},
-                   {'9', '0', 'A', 'B'}};*/
-     /*  char[][] matriz
-                = {{'1', '2', '3', '4'},
-                   {'5', '6', '7', '8'},
-                   {'9', '0', 'A', 'B'},
-                   {'C', 'D', 'E', 'F'}}; */
-
-     /*  char[][] matriz
-                = {{'1', '2', '3'},
-                {'4', '5', '6',},
-                {'7', '8', '9',},
-                {'0', 'A', 'B',}};
-                */
-
-         /* char[][] matriz
-                = {{'1', '2'},
-                {'3', '4'},
-                {'5', '6'},
-                {'7', '8'},
-                {'9', '0'},
-                {'A', 'B'}};
-          */
-
-        /*char[][] matriz = {{'1'},
-                {'2'},
-                {'3'},
-                {'4'},
-                {'5'},
-                {'6'},
-                {'7'},
-                {'8'},
-                {'9'},
-                {'0'},
-                {'A'}};*/
-
-        char[][] matriz  = {{'1', '2', '3'},
-                {'4', '5', '6'},
-                {'7', '8', '9'},
-                {'0', 'A', 'B'}};
-
-        String [] movements = {"RURU","DL","D","R"};
-        System.out.println(areMovementsValid(movements));
-       // System.out.println(isKeypadValid(matriz));
-        System.out.println(obtainCode(matriz, movements));
-
-    }
-
 
 }
 
